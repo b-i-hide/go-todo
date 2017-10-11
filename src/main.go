f82df499849cabf0ffb4073f9b1ae0268f952390
b-i-hide <b.i1993patriots@gmail.com>
2017-10-11 13:42:03 +0900

    initial commit

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/:path", Index)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+}
